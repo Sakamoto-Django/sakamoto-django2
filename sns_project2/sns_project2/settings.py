36a2874047422ec8f145d95e6dff83ab45167254
Ikuya Yamada <dalyson.roman345@gmail.com>
2020-02-29 22:44:44 +0900

    edit setting.py and urls.py

--- sns_project2/sns_project2/settings.py
+++ sns_project2/sns_project2/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 追加分
+    'toppage.apps.ToppageConfig'
 ]
 
 MIDDLEWARE = [
