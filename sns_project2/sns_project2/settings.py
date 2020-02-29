fe0e74c908e05d6ee8c8f1b5bd7b5772214abca8
Ikuya Yamada <dalyson.roman345@gmail.com>
2020-03-01 00:52:17 +0900

    edit setting.py

--- sns_project2/sns_project2/settings.py
+++ sns_project2/sns_project2/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'django.contrib.admin', 
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 追加分
-    'toppage.apps.ToppageConfig'
-    'accounts.apps.AccountsConfig'
+    'toppage.apps.ToppageConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
