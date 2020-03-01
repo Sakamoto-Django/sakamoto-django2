14ccd1dae11af5c8c801e0d91f14de9e3866766c
Ikuya Yamada <dalyson.roman345@gmail.com>
2020-03-01 12:30:18 +0900

    add mypage and inquiry

--- sns_project2/toppage/urls.py
+++ sns_project2/toppage/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'toppage'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('mypage/', views.MypageView.as_view(), name='mypage'),
+    path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
 ]
--- sns_project2/toppage/views.py
+++ sns_project2/toppage/views.py
@@ -6,3 +6,9 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
+
+class MypageView(generic.TemplateView):
+    template_name = 'mypage.html'
+
+class InquiryView(generic.TemplateView):
+    template_name = 'inquiry.html'
