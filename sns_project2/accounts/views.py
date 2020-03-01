ae49e786727cd349c415cd71a6758b2fb6a78b0b
坂本晋悟 <sakamotosusumusatoru@shingo-Mac-Pro.local>
2020-03-01 12:44:39 +0900

    create new files on accounts directory

--- sns_project2/accounts/views.py
+++ sns_project2/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login. authenticate
+from django.views.generic import CreateView
+from .forms import UserCreateForm
 
 # Create your views here.
+class Create_account(CreateView):
+    def post(self, request, *args, **kwargs):
+        form=UserCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            #read "username" from from
+            username=form.cleaned_data.get('usename')
+            #read "password1" from form
+            password1=form.cleaned_data.get('password1')
